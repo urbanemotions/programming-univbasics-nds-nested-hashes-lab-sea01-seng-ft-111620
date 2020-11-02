b0f481a6fecf04e04bc7c066462bfbe55713b4e8
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 19:36:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,7 +32,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  lookup_list = [assembled_list][2][:name]
+  lookup_list = [assembled_aoh][2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
