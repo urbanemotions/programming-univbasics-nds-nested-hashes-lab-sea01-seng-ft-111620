0a949ff28390c086abd3ccdda84bdaecb98c7778
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 19:58:52 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,17 +9,48 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
+  assembled_list = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G, 
+    BRUCE_G
+    ]
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  literal_list = [
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality" },
+    {:name => "Pat Monteseian", :occupation => "Staff" },
+    {:name => "Kate Gompert", :occupation => "None" },
+    {:name => "Bruce Green", :occupation => "Fan of Mildred" }
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
+  assembled_list = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G, 
+    BRUCE_G
+    ]
+    assembled_list[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  assembled_list = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G, 
+    BRUCE_G
+    ]
+    assembled_list[1][:name] ="Joelle van Dyne (PGOAT)"
+    p assembled_list
 end
