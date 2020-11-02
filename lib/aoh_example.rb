3cdbe2fcb3dede336a4067d9eb3aa492d4a852f2
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 19:46:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,13 +22,13 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal_list = [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => ""
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
-    literal_list
 end
 
 def aoh_lookup(aoh, row, key)
