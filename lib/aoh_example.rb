fadb15f3c7b8ba5925cc1b8e901a2585d2a5761d
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 19:53:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -28,7 +28,6 @@ def literal_aoh
     {:name => "Kate Gompert", :occupation => "None" },
     {:name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
-    literal_list.sort
 end
 
 def aoh_lookup(aoh, row, key)
