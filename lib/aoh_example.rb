68b4bb9579b22b65f36a71a985bf72985f364302
Sin Wildy <sw.wildy@gmail.com>
2020-11-02 19:48:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,11 +23,10 @@ def literal_aoh
   # held in the constants
   literal_list = [
     {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => ""
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality" },
+    {:name => "Pat Monteseian", :occupation => "Staff" },
+    {:name => "Kate Gompert", :occupation => "None" },
+    {:name => "Bruce Green", :occupation => "Fan of Mildred" }
     ]
 end
 
